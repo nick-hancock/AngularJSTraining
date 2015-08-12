290997f7c2f720ed35e689f87888447290730b7f
Nick Hancock <nick.hancock@jhc.co.uk>
2015-08-12 15:47:17 +0100

    Add karma unit tests for all delete functionality.

--- app/appTest.js
+++ app/appTest.js
@@ -2,9 +2,11 @@ describe('Test the todo controller', function() {
   beforeEach(module('todoApp'));
 
   var ctrl;
+  var originalTodos;
   
-  beforeEach(inject(function($controller) {
+  beforeEach(inject(function($controller, $filter) {
     ctrl = $controller('TodoListController');
+    originalTodos = angular.copy(ctrl.todos)
   }));
 
   it('should have todos available on load', function() {
@@ -22,10 +24,28 @@ describe('Test the todo controller', function() {
     ctrl.addTodo();
     
     expect(ctrl.todos.length).toBe(4);
-    expect(ctrl.todos[ctrl.todos.length - 1]).toEqual(
-      {text:text, done:false}
-    );
+    expect(ctrl.todos[ctrl.todos.length - 1]).toEqual({text:text, done:false});
     expect(ctrl.uncompleted()).toBe(3);
     expect(ctrl.todoText).toBe('');
   });
+
+  it('should delete all todos', function() {
+    ctrl.deleteAll();
+    expect(ctrl.todos.length).toBe(0);
+  });
+
+
+  it('should delete all completed todos', function($filter) {
+    ctrl.deleteCompleted();
+    expect(ctrl.todos.length).toBe(2);
+    expect(ctrl.todos).toEqual($filter('onlyUncompleted')(originalTodos, {done:false}))
+  });
+
+  it('should delete specific todos by index', function() {
+    ctrl.delete(1);
+    expect(ctrl.todos.length).toBe(2);
+    expect(ctrl.todos[0]).toEqual(originalTodos[0]);
+    expect(ctrl.todos[1]).toEqual(originalTodos[2]);
+  });
+
 });
